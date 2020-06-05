c3bdc53ef0dd8143e847a724b95e267a95517f53
chanho Jung <elvpfhhrm@gmail.com>
2020-06-05 23:43:53 +0900

    Edit Scripts(Oxalis, UIControl) and Object

--- Assets/Scripts/Oxalis.cs
+++ Assets/Scripts/Oxalis.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Oxalis : MonoBehaviour
 {
+    private float save_time = 0;
+    //for manage Exp
+    private float grow_Speed = 3.0f;
+    private float[] max=new float[5];// the max_exp of each stage
+    private int now_stage;
+    float now_Exp;
+    float Max_Exp;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        max[0] = 500.0f;
+        max[1] = 1200.0f;
+        max[2] = 2400.0f;
+        max[3] = 3600.0f;
+        max[4] = 5000.0f;
+        now_stage = PlayerPrefs.GetInt("now_stage",0);
+        now_Exp = PlayerPrefs.GetFloat("now_Exp",0);
+        Max_Exp = max[now_stage];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void fixedUpdate()
+    {
+        now_Exp += Time.deltaTime * grow_Speed;
+        save_time += Time.deltaTime;
+        if (save_time > 4.0f)
+        {
+            PlayerPrefs.SetInt("now_stage",this.now_stage);
+            PlayerPrefs.SetFloat("now_Exp",this.now_Exp);
+        }
+        //update left top UI Bar
+        UIControl.instance.SetValue(now_Exp / Max_Exp);
+    }
+
+    public float getExpPercentage()
+    {
+        return now_Exp / Max_Exp;
+    }
+
+    public void growSpeed_Up()
+    {
+        this.grow_Speed = this.grow_Speed * 2;
+    }
+
+    public void growSpeed_Origin()
+    {
+        this.grow_Speed = this.grow_Speed / 2;
     }
 }
--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static UIControl instance { get; private set; }
+    public Image mask;
+    private float originalSize;
+    private Oxalis oxalis;
+    
+    void Awake() 
+    {
+        instance=this; 
+    }
+    
     void Start()
     {
-        
+        originalSize=mask.rectTransform.rect.width;    
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetValue(float value) 
     {
-        
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,originalSize*value); 
     }
 }
