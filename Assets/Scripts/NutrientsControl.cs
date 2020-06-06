e1edc43e05d5534943be9f825b94df1d4c8473a5
chanho Jung <elvpfhhrm@gmail.com>
2020-06-07 00:05:45 +0900

    edit the NutrientsControl script
    when click the button it will show the Text that mean coolTime for reuse

--- Assets/Scripts/NutrientsControl.cs
+++ Assets/Scripts/NutrientsControl.cs
@@ -1,18 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Nutrients : MonoBehaviour
+using UnityEngine.UI;
+public class NutrientsControl : MonoBehaviour
 {
+    //for cool time setting
+    private float N_coolTime;
+    private bool N_usable;
+    //Button text
+    public Text button_Text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        N_usable = GetBool("N_usable");
+        button_Text.gameObject.SetActive(N_usable);
+        N_coolTime = PlayerPrefs.GetFloat("N_coolTime",0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void fixedUpdate()
+    {
+        PlayerPrefs.SetFloat("N_coolTime",this.N_coolTime);
+        SetBool("N_usable",N_usable);
+        if(N_usable==true)
+        {
+            button_Text.text = N_coolTime.ToString();
+            if (N_coolTime > 0)
+            {
+                N_coolTime -= Time.deltaTime;
+            }
+            else if (N_coolTime < 0)
+            {
+                N_coolTime = 0.0f;
+            }else if (N_coolTime == 0)
+            {
+                N_usable = false;
+            }
+        }
+    }
+
+    public void button_clicked()
+    {
+        if (N_usable == false)
+        {
+            N_usable = true; 
+            button_Text.gameObject.SetActive(N_usable); 
+        }
+    }
+    //for save and loat the bool date
+    public void SetBool(string key, bool state)
+    {
+        PlayerPrefs.SetInt(key, state ? 1 : 0);
+    }
+
+    public bool GetBool(string key)
+    {
+        int value = PlayerPrefs.GetInt(key,0);
+        if (value == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
