6845dde79e11414a7a8dd5bef233428e244c41a4
jonghyeon_Choi <cc64265112@gmail.com>
2020-06-10 10:44:02 +0900

    a

--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public static UIControl instance{get;private set;}
+	public Image mask;
+	private float originalSize;
+	
+    // Awake is called when the program started
+    void Awake()
     {
-        
+        instance=this;
     }
-
+	//Start is called before the first frame Update
+	void Start()
+	{
+		originalSize=mask.rectTransform.rect.width;
+	}
+	public void SetValue(float value)
+	{
+	mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,originalSize*value);
+	}
     // Update is called once per frame
     void Update()
     {
