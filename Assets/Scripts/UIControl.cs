42fb1ddf35b3979a1c6e3701ec83068c9ee4f461
jonghyeon_Choi <cc64265112@gmail.com>
2020-06-10 10:46:40 +0900

    aa

--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -5,6 +5,24 @@ using UnityEngine.UI;
 
 public class UIControl : MonoBehaviour
 {
+<<<<<<< HEAD
+    public static UIControl instance { get; private set; }
+    public Image mask;
+    private float originalSize;
+    private Oxalis oxalis;
+    
+    void Awake() 
+    {
+        instance=this; 
+    }
+    
+    void Start()
+    {
+        originalSize=mask.rectTransform.rect.width;    
+    }
+
+    public void SetValue(float value) 
+=======
 	public static UIControl instance{get;private set;}
 	public Image mask;
 	private float originalSize;
@@ -25,7 +43,8 @@ public class UIControl : MonoBehaviour
 	}
     // Update is called once per frame
     void Update()
+>>>>>>> 06633eaa9cfdf659d728417686fb7676881b7376
     {
-        
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,originalSize*value); 
     }
 }
