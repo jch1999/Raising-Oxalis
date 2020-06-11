2b4b21c2558d0169cf02a0b688f380dd9e03dd3b
chanho Jung <elvpfhhrm@gmail.com>
2020-06-11 21:20:32 +0900

    create Exit script

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+   //when the eixt_button is clicked
+   public void Exit()
+   {
+      PlayerPrefs.Save();
+      Application.Quit();
+   }
+}
