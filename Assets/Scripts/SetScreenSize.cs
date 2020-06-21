135727ab090a7af70c9c98723938905a5baf23bf
chanho Jung <elvpfhhrm@gmail.com>
2020-06-21 12:56:04 +0900

    프로그램 실행 시의 화면 크기 설정 및 빌드

---
+++ Assets/Scripts/SetScreenSize.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetScreenSize : MonoBehaviour
+{
+    void Awake()
+    {
+        Screen.SetResolution(480, 640, false);
+    }
+}
--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-   
     // Update is called once per frame
     void Update()
     {
